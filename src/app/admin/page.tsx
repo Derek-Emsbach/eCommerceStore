42de34e279440edd0ab89faf51e805cde106e9b4
Derek-Emsbach <demsbach92@gmail.com>
2024-04-13 22:37:49 -0600

    fixed format

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,12 @@
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function AdminDashboard() {
     return <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         <Card>
-            <CardHeader>Sales</CardHeader>
-            <CardDescription>desc</CardDescription>
+            <CardHeader>
+                <CardTitle>Sales</CardTitle>
+                <CardDescription>desc</CardDescription>
+            </CardHeader>
             <CardContent>
                 <p>text</p>
             </CardContent>
