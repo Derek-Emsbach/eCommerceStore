8b7fdce27d000a35f9670bbbb6bda1e9a1f8aa46
Derek-Emsbach <demsbach92@gmail.com>
2024-04-13 22:36:04 -0600

    Sales card added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,13 @@
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+
 export default function AdminDashboard() {
     return <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        
+        <Card>
+            <CardHeader>Sales</CardHeader>
+            <CardDescription>desc</CardDescription>
+            <CardContent>
+                <p>text</p>
+            </CardContent>
+        </Card>
     </div>
 }
